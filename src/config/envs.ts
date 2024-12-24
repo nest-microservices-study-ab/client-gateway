14bbd6ed0bf75b0af686abf14cf6ebb7d6d9a72b
alejoboga20 <alejoboga19@gmail.com>
2024-12-24 11:31:42 -0300

    connect product microservice

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,11 +3,16 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+
+  PRODUCTS_MICROSERVICES_HOST: string;
+  PRODUCTS_MICROSERVICES_PORT: number;
 }
 
 const envVarsSchema = joi
   .object({
     PORT: joi.number().required(),
+    PRODUCTS_MICROSERVICES_HOST: joi.string().required(),
+    PRODUCTS_MICROSERVICES_PORT: joi.number().required(),
   })
   .unknown(true);
 
@@ -19,4 +24,6 @@ const envVars = value as EnvVars;
 
 export const envs = {
   port: envVars.PORT,
+  productMicroserviceHost: envVars.PRODUCTS_MICROSERVICES_HOST,
+  productMicroservicePort: envVars.PRODUCTS_MICROSERVICES_PORT,
 };
