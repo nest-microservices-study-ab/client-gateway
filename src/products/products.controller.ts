7ba8bf5684543c7878cd88924ceae14103b5a0f5
alejoboga20 <alejoboga19@gmail.com>
2024-12-26 15:42:33 -0300

    update product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -15,6 +15,7 @@ import { catchError } from 'rxjs';
 import { PaginationDto } from 'src/common';
 import { PRODUCT_SERVICE } from 'src/config';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -67,8 +68,17 @@ export class ProductsController {
   }
 
   @Patch(':id')
-  updateProduct(@Param('id') id: string, @Body() updateProductDto: any) {
-    return `This action updates a product with id: ${id} with ${JSON.stringify(updateProductDto)}`;
+  updateProduct(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsClient
+      .send({ cmd: 'update_product' }, { id, ...updateProductDto })
+      .pipe(
+        catchError((error) => {
+          throw new RpcException(error);
+        }),
+      );
   }
 
   @Delete(':id')
